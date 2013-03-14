f9d80a6b456f8a8a4d93776e646edc03b2b21812
eskerda <eskerda@gmail.com>
2013-03-14 18:18:04 +0100

    Add test code simulating the gamepad
    - Presses all possible button combinations
    - Does clockwise barrel rolls \o/

---
+++ arduines-usb/gamepad-test.ino
@@ -0,0 +1,68 @@
+/* Arduino USB Joystick HID demo */
+
+/* Author: Darran Hunt
+ * Released into the public domain.
+ */
+ 
+/* Author: eskerda
+ * Released into the wild
+ */
+
+int PADS[][2] = {
+  {0,0},
+  {0,1},
+  {1,1},
+  {1,0},
+  {1,-1},
+  {0,-1},
+  {-1,-1},
+  {-1,0},
+  {-1,1},
+  {0,1},
+  {0,0}
+};
+
+int AXIS_VAL = 100;
+int N_PADS = sizeof(PADS) / (sizeof(int)*2);
+struct {
+    int8_t x;
+    int8_t y;
+    uint8_t buttons;
+    uint8_t rfu; 	/* reserved for future use */
+} joyReport;
+
+void setup();
+void loop();
+
+void setup() 
+{
+    Serial.begin(115200);
+    delay(5000);
+}
+
+void loop() 
+{
+    joyReport.buttons = 0;
+    joyReport.x = 0;
+    joyReport.y = 0;
+    joyReport.rfu = 0;
+    
+    /* Make the pad do a clockwise barrel roll */
+    for (int i = 0; i < N_PADS; i++){
+      joyReport.x = PADS[i][0] * AXIS_VAL;
+      joyReport.y = PADS[i][1] * - AXIS_VAL;
+      Serial.write((uint8_t *)&joyReport, 4);
+      delay(250);
+    }
+    
+    /* Press every combination ever of buttons 
+     * That is.. button per bit, everything from
+     * 0x00 to 0x0F
+     */
+    for (int i = 0; i <= 0x0F; i++){
+      joyReport.buttons = i;
+      Serial.write((uint8_t *)&joyReport, 4);
+      delay(250);
+    }     
+    delay(1000);
+}
